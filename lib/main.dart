9665c2e8036f27ea836eb740f3d9788b774a25bb
unknown <garam103@naver.com>
2024-01-06 06:40:40 +0900

    theme management

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,26 @@ void main()
 {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'sunflower',
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            color: Colors.white,
+            fontFamily: 'parisienne',
+            fontSize: 80.0,
+          ),
+          headline2: TextStyle(
+              color: Colors.white,
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700),
+          bodyText1: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0),
+          bodyText2: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0),
+        )
+      ),
       home: MainScreen(),
     )
   );
--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
@@ -79,6 +78,8 @@ class _TopPart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
     final now = DateTime.now();
 
     return Expanded(
@@ -86,26 +87,16 @@ class _TopPart extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Text('U&I',
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'parisienne',
-                fontSize: 80.0,
-              )),
+              style: textTheme.headline1),
           Column(
             children: [
               Text(
                 '우리 처음 만난 날',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'sunflower',
-                    fontSize: 30.0),
+                style: textTheme.bodyText1
               ),
               Text(
                 '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'sunflower',
-                    fontSize: 20.0),
+                style: textTheme.bodyText2
               ),
             ],
           ),
@@ -116,11 +107,7 @@ class _TopPart extends StatelessWidget {
               color: Colors.red),
           Text(
             'D+${DateTime(now.year, now.month, now.day).difference(selectedDate).inDays + 1}',
-            style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'sunflower',
-                fontSize: 50.0,
-                fontWeight: FontWeight.w700),
+            style: textTheme.headline2
           ),
         ],
       ),
