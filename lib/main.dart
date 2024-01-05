f859dbf8497b7ee1014ebeda26dc3bf08ace3595
unknown <garam103@naver.com>
2024-01-05 19:08:55 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:dday/screen/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      home: MainScreen(),
+    )
+  );
+}
---
+++ lib/screen/main_screen.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              _TopPart(),
+              _bottomPart(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TopPart extends StatefulWidget {
+  const _TopPart({super.key});
+
+  @override
+  State<_TopPart> createState() => _TopPartState();
+}
+
+class _TopPartState extends State<_TopPart> {
+  DateTime selectedDate = DateTime(
+    //when I wanna start to count time(date) at 00:00:00 am
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final now = DateTime.now();
+
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text('U&I',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'parisienne',
+                fontSize: 80.0,
+              )),
+          Column(
+            children: [
+              Text(
+                '우리 처음 만난 날',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'sunflower',
+                    fontSize: 30.0),
+              ),
+              Text(
+                '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'sunflower',
+                    fontSize: 20.0),
+              ),
+            ],
+          ),
+          IconButton(
+              iconSize: 60.0,
+              onPressed: () {
+                showCupertinoDialog(
+                  context: context,
+                  barrierDismissible: true,
+                  builder: (context) {
+                    return Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        color: Colors.white,
+                        height: 300.0,
+                        child: CupertinoDatePicker(
+                          mode: CupertinoDatePickerMode.date,
+                          initialDateTime: selectedDate,
+                          maximumDate: DateTime(
+                            now.year,
+                            now.month,
+                            now.day,
+                          ),
+                          onDateTimeChanged: (DateTime date) => setState(() {
+                            selectedDate = date;
+                          }),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+              icon: Icon(Icons.favorite),
+              color: Colors.red),
+          Text(
+            'D+${DateTime(now.year, now.month, now.day).difference(selectedDate).inDays + 1}',
+            style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'sunflower',
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _bottomPart extends StatelessWidget {
+  const _bottomPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child: Image.asset('asset/img/couple.png'));
+  }
+}
